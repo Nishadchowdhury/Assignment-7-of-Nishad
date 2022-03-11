bb1250e7821ff1fb36488097ed18a61ff6c51bd7
Nishadchowdhury <Nishadhj111@gmail.com>
2022-03-11 16:29:19 +0600

    2 answer add

--- js/app.js
+++ js/app.js
@@ -35,16 +35,20 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "answers" ).style.display = "block";
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "answers" ).style.display = "none";
+
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById( "answers" ).style.display = "none";
 
         displayReportedPosts();
     }
